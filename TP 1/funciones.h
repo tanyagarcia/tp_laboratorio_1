40f14d602b24f338d6845c31b0febcd69258a357
Tanya García <garcia.tanya0204@gmail.com>
2017-04-07 11:43:14 -0300

    Trabajo Práctico 1
    Archivos de TP1

---
+++ TP 1/funciones.h
@@ -0,0 +1,11 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+int sumar(int, int);
+int restar(int, int);
+int division(int, int);
+int multiplicacion(int, int);
+int factorial(int);
+
+#endif // FUNCIONES_H_INCLUDED
+
